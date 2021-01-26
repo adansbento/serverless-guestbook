931990e246c967c51bbd5d2d0c5b9719fbe2cb78
Adans Bento <adansbento@gmail.com>
2021-01-25 21:42:02 -0300

    Update guestbook.js
    - set my host

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://34eeabfe.us-south.apigw.appdomain.cloud/user';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
